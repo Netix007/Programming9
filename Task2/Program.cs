2cd84db508ede0185136eb2643fa82cd4cf51eeb
Aleksandr <kino_mac@mail.ru>
2022-03-06 10:49:15 +0300

    Добавлено решение 2-ой задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 69: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetSum(int m, int n, int sum)
+{
+    sum = sum + m;
+    if (m < n)
+    {
+        m++;
+        sum = GetSum(m,n,sum);
+    }
+    return sum;    
+}
+
+int InputNumber(string name)
+{
+    int number = 0;
+    bool isError = true;
+    while (isError)
+    {
+        Console.Write($"Введите {name} число: ");
+        string text = Console.ReadLine();
+        if (Int32.TryParse(text, out number) && number > 0)
+            {isError = false;}
+    }
+    return number;
+}
+
+int m = InputNumber("первоe");
+int n = InputNumber("второе");
+int sum = 0;
+if (m > n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+sum = GetSum(m,n,sum);
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n}: {sum}");
